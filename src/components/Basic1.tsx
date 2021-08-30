fe349d09775aac7067fdc095d0b926a91cdfae97
shitikakei <shitikakei@gmail.com>
2021-08-31 03:52:47 +0900

    commit

--- src/components/Basic1.tsx
+++ src/components/Basic1.tsx
@@ -1,26 +1,41 @@
 import React, { useState } from "react";
 
-const Basic1 = () => {
-  let [count, setCount] = useState(0);
-  //   まず基本的なHandler
-  //   const BasicCountHandler = () => {
-  //     setCount((count += 1));
-  //   };
-  //   次に前回のレンダリング結果を参照する形でのHandler
-  const PreviousCountHandler = () => {
-    setCount((count += 1));
-    // つまり1回のレンダリングでまずcount = 1となりその結果をprev-で参照して+1するので、最終的にcount = 2
-    setCount((prevCount) => prevCount + 1);
+const Basic1: React.FC = () => {
+  // Objectの例
+  interface Product {
+    name: string;
+    price: number | string;
+  }
+  const [product, setProducts] = useState<Product>({ name: "", price: "" });
+
+  const onChangeNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //   Objectの一部を書き換える場合は...Objectで記述する。すると要素ごとに分解されるので書き換えたいところだけ以下のようにする
+    setProducts({ ...product, name: e.target.value });
+  };
+  const onChangePriceHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //   Objectの一部を書き換える場合は...Objectで記述する。すると要素ごとに分解されるので書き換えたいところだけ以下のようにする
+    setProducts({ ...product, price: e.target.value });
   };
 
   return (
     <>
       <div>
-        <button onClick={PreviousCountHandler}>
-          {/* 同じことをjsxでやるなら以下 */}
-          {/* <button onClick={() => setCount((prevCount) => prevCount + 1)}> */}
-          Count {count}
-        </button>
+        <form action="">
+          <input
+            type="text"
+            value={product.name}
+            onChange={onChangeNameHandler}
+          />
+          <br />
+          <input
+            type="text"
+            value={product.price}
+            onChange={onChangePriceHandler}
+          />
+        </form>
+
+        <h1>Product Name is {product.name}</h1>
+        <h1>Product Price is {product.price}</h1>
       </div>
     </>
   );
